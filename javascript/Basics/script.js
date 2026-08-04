01e3768f975d5bf8b7086d134c4dc28a78b04dec
Jatin Rana <jatinrana.code@gmail.com>
2026-08-04 11:27:31 +0530

    Reorganize project structure

---
+++ javascript/Basics/script.js
@@ -0,0 +1,125 @@
+// console.log("This is Normal text");
+// console.warn("This is Warning");
+// console.error("This is Warning");
+// var a = 100;
+// console.log(a);
+// console.table([10,20,30])
+
+// var b = 3;
+// console.log("Value of a is", a, "Value of b", b);
+
+// console.log(a, b);
+// console.log(a * b);
+// console.log(a % b);
+
+// var t;
+
+// console.log(t);
+// var y = 50
+// console.log(typeof(y));
+
+// var s = null
+// alert('helooooo')
+// var message = confirm('Do Your want to continue')
+// console.log(message);
+
+//  var age = prompt('enter your age')
+//  console.log(age);
+
+//  var x = '35'
+//  var v = '30' 
+// console.log(x+v);
+
+//  var aa = '30'
+
+//  var bb = Number(aa)
+//  console.log(bb);
+//  console.log(typeof(bb));
+
+
+
+
+
+// var maths = Number(prompt('Enter Your Marks in Maths'))
+// var phy = Number(prompt('Enter Your Marks in PHYSICS'))
+// var chem = Number(prompt('Enter Your Marks in CHemistry'))
+// console.log("maths",maths, "physics",phy,"chemistry",chem);
+
+// var avg = Number((maths+chem+phy)/3)
+// console.log("avg",avg);
+
+
+//  if(avg >= 85){
+//     console.log('100%scholarship');
+
+    
+//  }else if(avg>= 70){
+//     console.log('50%Scholarship');
+    
+//  }
+//  else{
+//     console.log('No Scholarship');
+    
+//  }
+
+ 
+
+//  var num = Number(prompt('Enter number'))
+
+//  var a = 1;
+//  while(a<=10){
+//    console.log(num+' X '+a+'='+num*a);
+//    a++
+   
+//  }
+
+
+// for(var n = 0 ; n<10 ; n++){
+//    console.log("hiiiii", n);
+// }
+
+
+
+for(var a = 1 ; a<=10; a++){
+   console.log(a);
+   if(a == 5){
+      continue;
+
+
+   }
+ 
+   
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ javascript/Functions/script.js
@@ -0,0 +1,26 @@
+function greet(){
+    console.log('good morning....');
+
+}
+greet()
+greet()
+
+function mul(a,b){
+    console.log(a*b)
+} 
+mul(20,40)
+
+function abc(user,age){
+    console.log('Good Morning..',user);
+    if(age>=18){
+        console.log('Your are allowed in Party')
+
+    }else{
+        console.log('You are not allowed in Party');
+    }
+}
+abc('Rahul',23)
+abc('Sohit',17)
+
+var xyz = (a,b) => console.log('helooooo ',a,b);
+xyz('Arnav','and tinku')
