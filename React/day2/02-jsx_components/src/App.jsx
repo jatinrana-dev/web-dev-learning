670f6f01e64fcab95663423f473128ecad271562
Jatin Rana <jatinrana.code@gmail.com>
2026-08-25 19:57:47 +0530

    jsx and components

---
+++ React/day2/02-jsx_components/src/App.jsx
@@ -0,0 +1,18 @@
+import About from "./about"
+let App = () => {
+  
+  return (
+    <main>
+      <h1>helooo</h1>
+      <h2>guysss</h2>
+      <h3>how rr uuuu</h3>
+      <About width = "600" name = "jatin" age = "20" >
+
+      </About>
+
+    </main>
+   
+  );
+}
+
+export default App;
---
+++ React/day2/02-jsx_components/src/about.jsx
@@ -0,0 +1,10 @@
+let About = ({width, name, age}) => {
+console.log(width,name,age)
+
+ return(
+    <nav>
+        <h1>this is form about.jsx</h1>
+    </nav>
+ );
+};
+export default About ;
