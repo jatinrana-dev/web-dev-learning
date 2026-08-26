15500de6118b6c5f5c3451a02b3dcc63a98c156b
Jatin Rana <jatinrana.code@gmail.com>
2026-08-26 23:25:40 +0530

    jsx and components

--- React/day2/02-jsx_components/src/App.jsx
+++ React/day2/02-jsx_components/src/App.jsx
@@ -4,8 +4,9 @@ let App = () => {
   return (
     <main>
       <h1>helooo</h1>
-      <h2>guysss</h2>
-      <h3>how rr uuuu</h3>
+      <h2>guyss</h2>
+      <h3>how rr u</h3>
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam iste sequi accusantium sapiente animi aliquid fugiat delectus libero maiores expedita soluta, veniam ipsa rem dolorem totam provident, voluptatibus mollitia vel?</h1>
       <About width = "600" name = "jatin" age = "20" >
 
       </About>
