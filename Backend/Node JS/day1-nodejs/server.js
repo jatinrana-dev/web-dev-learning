a5fc0fd377834c9e3f4e85297b34ec51c9e90efb
Jatin Rana <jatinrana.code@gmail.com>
2026-09-12 19:14:58 +0530

    created server using http

--- Backend/Node JS/day1-nodejs/server.js
+++ Backend/Node JS/day1-nodejs/server.js
@@ -1,11 +1,9 @@
 let http = require("http");
 
-let server = http.createServer((req,res) => {
-    console.log("heloo i am server");
-    res.end("i have proccesed your request");
+let server = http.createServer((req, res) =>{
+    console.log("the server is started")
+    res.end("the request is accepted and resond is being send")
 
 })
 
-server.listen(3000, () => {
-    console.log('server is running in the port 3000')
-})
+server.listen(3000);
