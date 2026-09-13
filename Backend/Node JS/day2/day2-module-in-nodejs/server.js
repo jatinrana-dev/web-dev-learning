1dd4034ebcbdae1c73b857746081ed6bde133317
Jatin Rana <jatinrana.code@gmail.com>
2026-09-13 12:26:07 +0530

    Export and Import in Common JS and ES6

--- Backend/Node JS/day2/day2-module-in-nodejs/server.js
+++ Backend/Node JS/day2/day2-module-in-nodejs/server.js
@@ -2,6 +2,6 @@
 // const math = require('./index.js')
 // console.log(math.sum(4,7))
 
-//ES6(modern js...)
+//ES6(modern js......)
 import sum ,{sub,mul} from './index.js'
 console.log(sub(20,4),mul(7,5));
